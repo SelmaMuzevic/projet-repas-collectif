860f821beabb9f1ef0bcd2413b3a869701797986
selma <selma@gmail.com>
2017-06-30 12:20:12 +0200

    Recuperer les donnees de formulaire et les traiter

--- control/connexion.php
+++ control/connexion.php
@@ -1,7 +1,14 @@
 <?php
 
 /* 
-
+recuperer les donnes et les traiter
  */
+function traiterConnect() {
+    if(isset($_POST['pseudo']) && isset($_POST['mdp']) ) {
+    $pseudo = $_POST['pseudo'];
+    $mdp = $_POST['mdp'];
+    var_dump($_POST);
+    }
 
+}
 ?>
--- control/inscription.php
+++ control/inscription.php
@@ -4,3 +4,13 @@
   PHP recuperation des donnees de formulaire et le traitement
  */
 
+
+function recupererForm() {
+    if(isset($_POST['pseudo']) && isset($_POST['mdp']) ) {
+    $pseudo = $_POST['pseudo'];
+    $mdp = $_POST['mdp'];
+    var_dump($_POST);
+    }
+
+}
+
