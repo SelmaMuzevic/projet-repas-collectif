dc5c1bf46b2661b5c2e63d765989c8e64c86d33a
selma <selma@gmail.com>
2017-07-10 14:25:38 +0200

    Dans les fichiers php creation des liens de differentes directions

--- control/events.php
+++ control/events.php
@@ -1,32 +1,40 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-session_start();
 
 include_once '../model/Evenement.php';
 include_once '../model/Database.php';
 
+session_start();
+
+
 $data = new Database();
 
 
-if(!empty($_POST['nom']) && !empty($_POST['type']) && !empty($_POST['dateTime'])
-        && !empty($_POST['lieu']) && !empty($_POST['adresse'])){
+
+if (!empty($_POST['nom']) && !empty($_POST['type']) && !empty($_POST['dateTime']) && !empty($_POST['lieu']) && !empty($_POST['adresse'])) {
+
+    $nom = $_POST['nom'];
+    $type = $_POST['type'];
+    $dateTime = $_POST['dateTime'];
+    $lieu = $_POST['lieu'];
+    $adresse = $_POST['adresse'];
+
+
+    $evenements = new Evenement($nom, $type, $dateTime, $lieu, $adresse);
+    var_dump($evenements);
+    echo '<br/>';
+
+    $data->creerEvenement($evenements);
     
-            $nom = $_POST['nom'];
-            $type =$_POST['type'];
-            $dateTime = $_POST['dateTime'];
-            $lieu = $_POST['lieu'];
-            $adresse = $_POST['adresse'];
-    
-     
-     $evenement = new Evenement($nom, $type, $dateTime, $lieu, $adresse);
-              var_dump($evenement);
-     $data->stockerEvenement($evenement);
+    header('location: ../vue/affiche-evenements.php');
+} else {
+    echo 'Faut remplir tous les champs !!!!';
 }
 
 
